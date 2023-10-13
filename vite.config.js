b3e81ba46d612c8e15a9d7951ddcb3bc85883ae8
Igor Fedyanin <logowaz25@yandex.ru>
2023-10-14 00:52:30 +0300

    12 ПР все работает,на проверку 2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/mesto-react/',
-    base: '/',
+  base: '/mesto-react-auth/',
+    // base: '/',
   mode: 'hash',
 })
 
